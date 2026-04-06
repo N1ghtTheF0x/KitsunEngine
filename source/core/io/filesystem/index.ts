0fc094e7222bd8b688de55bb3a837903af9d5eaa
N1ghtTheF0x <kilo.night.prower@gmail.com>
2026-04-06 04:53:05 +0200

    string, json reading/writing

--- source/core/io/filesystem/index.ts
+++ source/core/io/filesystem/index.ts
@@ -1,3 +1,4 @@
+import { textDecoder, textEncoder } from "@utilities/string"
 import EngineCoreIO from ".."
 import FileSystemProvider, { IReadonlyFileSystemProvider, IWriteOnlyFileSystemProvider } from "./provider"
 import fetchReadProvider from "./read/fetch"
@@ -10,24 +11,42 @@ class EngineCoreIOFileSystem implements FileSystemProvider
     public constructor(io: EngineCoreIO)
     {
         this.io = io
-        this.addReadProvider(fetchReadProvider)
+        this.pushReadProvider(fetchReadProvider)
     }
-    public addProvider(...providers: Array<FileSystemProvider>): this
+    public pushProvider(...providers: Array<FileSystemProvider>): this
     {
         for(const provider of providers)
             this
-            .addReadProvider(provider)
-            .addWriteProvider(provider)
+            .pushReadProvider(provider)
+            .pushWriteProvider(provider)
         return this
     }
-    public addReadProvider(...providers: Array<IReadonlyFileSystemProvider>): this
+    public unshiftProvider(...providers: Array<FileSystemProvider>): this
+    {
+        for(const provider of providers)
+            this
+            .unshiftReadProvider(provider)
+            .unshiftWriteProvider(provider)
+        return this
+    }
+    public pushReadProvider(...providers: Array<IReadonlyFileSystemProvider>): this
     {
         this._readProvider.push(...providers)
         return this
     }
-    public addWriteProvider(...providers: Array<IWriteOnlyFileSystemProvider>): this
+    public unshiftReadProvider(...providers: Array<IReadonlyFileSystemProvider>): this
+    {
+        this._readProvider.unshift(...providers)
+        return this
+    }
+    public pushWriteProvider(...providers: Array<IWriteOnlyFileSystemProvider>): this
     {
         this._writeProvider.push(...providers)
+        return this
+    }
+    public unshiftWriteProvider(...providers: Array<IWriteOnlyFileSystemProvider>): this
+    {
+        this._writeProvider.unshift(...providers)
         return this
     }
     public async readFile(filepath: string): Promise<ArrayBuffer>
@@ -51,6 +70,14 @@ class EngineCoreIOFileSystem implements FileSystemProvider
 
         throw new AggregateError(errors,`failed to read '${filepath}'`)
     }
+    public async readTextFile(filepath: string): Promise<string>
+    {
+        return this.readFile(filepath).then(buffer => textDecoder.decode(buffer))
+    }
+    public async readJsonFile(filepath: string): Promise<unknown>
+    {
+        return JSON.parse(await this.readTextFile(filepath))
+    }
     public async writeFile(filepath: string,data: ArrayBuffer): Promise<void>
     {
         const errors: Array<Error> = []
@@ -71,6 +98,14 @@ class EngineCoreIOFileSystem implements FileSystemProvider
         }
 
         throw new AggregateError(errors,`failed to write '${filepath}'`)
+    }
+    public async writeTextFile(filepath: string,data: string): Promise<void>
+    {
+        return this.writeFile(filepath,textEncoder.encode(data).buffer)
+    }
+    public async writeJsonFile(filepath: string,data: unknown): Promise<void>
+    {
+        return this.writeTextFile(filepath,JSON.stringify(data))
     }
 }
 
