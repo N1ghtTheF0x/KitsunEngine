2eb1c424e0f1b2e32b5a3eb8ec8a71075aceebc6
N1ghtTheF0x <kilo.night.prower@gmail.com>
2026-04-06 04:53:22 +0200

    adjusted update loop

--- source/core/index.ts
+++ source/core/index.ts
@@ -2,15 +2,18 @@ import KitsunEngine from ".."
 import { IInit, IPostUpdate, IUpdate } from "@utilities/types"
 import EngineCoreIO from "./io"
 import EngineCoreWindow from "./io/window"
+import EngineCoreConfig from "./config"
 
 class EngineCore implements IUpdate, IInit, IPostUpdate
 {
     public readonly engine: KitsunEngine
+    public readonly config: EngineCoreConfig
     public readonly window: EngineCoreWindow
     public readonly io: EngineCoreIO
     public constructor(engine: KitsunEngine)
     {
         this.engine = engine
+        this.config = new EngineCoreConfig(this)
         this.window = new EngineCoreWindow(this)
         this.io = new EngineCoreIO(this)
     }
@@ -20,6 +23,7 @@ class EngineCore implements IUpdate, IInit, IPostUpdate
     }
     public update(): void
     {
+        this.window.update()
         this.io.update()
     }
     public postUpdate(): void
